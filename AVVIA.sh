527a9c337a83d8cbf1dd092ea2a0e6544c5323aa
wildlux <wildlux@gmail.com>
2025-10-24 13:20:43 +0200

    Add .gitignore to exclude __pycache__ and db.sqlite3, update AVVIA.sh

--- AVVIA.sh
+++ AVVIA.sh
@@ -1,1 +1,3 @@
+# Applicazione di studio personale per Django - Barber Shop Management
+# Repository: https://github.com/wildlux/Crazy_BARBER_SHOP
 . ./venv/bin/activate && python manage.py runserver
